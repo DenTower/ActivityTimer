50f8d4a9cd181b4e1a30fcccec1e295495406210
DenTower <denis.zadorin.den@gmail.com>
2024-06-09 14:00:50 +0300

    fixed an error with flag in pending intent add required foreground service type in manifest

--- app/src/main/java/com/example/activitytimer/data/MainDb.kt
+++ app/src/main/java/com/example/activitytimer/data/MainDb.kt
@@ -1,7 +1,6 @@
 package com.example.activitytimer.data
 
 import android.content.Context
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -10,10 +9,7 @@ import androidx.room.RoomDatabase
     entities = [
         Entity::class
     ],
-    version = 2,
-    autoMigrations = [
-        AutoMigration(from = 1, to = 2)
-    ]
+    version = 1
 )
 abstract class MainDb : RoomDatabase() {
     abstract val dao: DataAccessObject
